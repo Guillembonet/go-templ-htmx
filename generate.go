f44615036719c90fbf83933864ca3d7790502f0f
Matsuuu <huhta.matias@gmail.com>
2025-11-25 07:16:09 +0200

    Update tailwind configuration to v4 compatible setup

--- generate.go
+++ generate.go
@@ -2,4 +2,4 @@ package go_templ_htmx
 
 //go:generate go tool templ generate
 
-//go:generate npx tailwindcss -c ./tailwind.config.js -i ./views/assets/css/input.css -o ./views/assets/css/tailwind.css --minify
+//go:generate npx @tailwindcss/cli -i ./views/assets/css/input.css -o ./views/assets/css/tailwind.css --minify
